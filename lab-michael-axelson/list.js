8d4473c8205cfaac45889bf1178c725d068195ed
Michael Axelson <axelmichael11@gmail.com>
2017-06-19 10:42:28 -0700

    working on slice test, starting this reduce function

--- lab-michael-axelson/list.js
+++ lab-michael-axelson/list.js
@@ -10,13 +10,46 @@ List.prototype.push = function(value) {
 };
 
 List.prototype.pop = function() {
-  // if (this.length < 1) return
+  if (this.length < 1) return
   let result = this[this.length-1];
   delete this[this.length -1];
   this.length--;
   return result;
 };
 
-// List.prototype.map = function(callback) {
-//
-// }
+List.prototype.map = function(callback) {
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+
+  }
+  return newArray;
+};
+
+List.prototype.filter = function(callback) {
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]))
+      newArray.push(this[i]);
+  }
+  return newArray;
+};
+
+
+List.prototype.slice = function(start, finish) {
+  let newArray = [];
+  for (let i=0; i<this.length; i++) {
+    if (start<this[i]<=finish)
+      newArray.push(this[i]);
+  }
+  return newArray;
+};
+
+// List.prototype.reduce = function(acc, prev, curr) {
+//   let newArray = [];
+//   for (let i=0; i<this.length; i++) {
+//     if (start<=this[i]<=finish)
+//       newArray.push(this[i]);
+//   }
+//   return newArray;
+// };
--- lab-michael-axelson/test/test.js
+++ lab-michael-axelson/test/test.js
@@ -10,6 +10,7 @@ describe('testing list', () => {
     expect(list.length).toEqual(0);
   });
 
+
   describe('testing push', () => {
     it('should append a value to the list', () => {
       let list = new List();
@@ -22,25 +23,97 @@ describe('testing list', () => {
       expect(list.length).toEqual(2);
     });
   });
+
+
   describe('testing pop', () => {
     it('should pop off a value from the list', () => {
       let list = new List();
+
       list.push(1);
       list.push(2);
       list.push(3);
-
 
       let result = list.pop();
       expect(result).toEqual(3);
       expect(list.length).toEqual(2);
       expect(list[2]).toBe(undefined);
 
-      list = new list();
+
+      // console.log(list);
+      list = new List();
       result = list.pop();
       expect(result).toEqual(undefined);
-      expect(list.length).toEqual(1);
-      expect(list[2]).toBe(0);
+      expect(list.length).toEqual(0);
     });
   });
-  // describe('testing pop')
+
+
+  describe('testing map', () => {
+    it('should add each value in the array by two...', () => {
+      let list = new List();
+
+      list.push(1);
+      list.push(2);
+      list.push(3);
+      // console.log(list);
+      let result = list.map((num) => num + 2);
+      expect(result.length).toEqual(3);
+      expect(result[0]).toEqual(3);
+      expect(result[1]).toEqual(4);
+      expect(result[2]).toEqual(5);
+
+    });
+  });
+  describe('testing filter', () => {
+    it('should filter out every value less than 4', () => {
+      let list = new List();
+
+      list.push(1);
+      list.push(2);
+      list.push(3);
+      list.push(4);
+
+      let result = list.filter((value) => {
+        if (value > 2)
+          return value;
+      });
+
+      expect(result.length).toEqual(2);
+      expect(result[0]).toEqual(3);
+      expect(result[1]).toEqual(4);
+      expect(result[4]).toEqual(undefined);
+      expect(result[5]).toEqual(undefined);
+
+    });
+  });
+  describe('testing slice', () => {
+    it('should slice this example list from everything between 4 and 7...', () => {
+
+      let list = new List();
+
+      list.push(1);
+      list.push(2);
+      list.push(3);
+      list.push(4);
+      list.push(5);
+      list.push(6);
+      list.push(7);
+      list.push(8);
+      console.log(list);
+      expect(list.length).toEqual(4);
+      let result = list.slice(3,7);
+      console.log(result.length);
+      expect(result.length).toEqual(4);
+      expect(result[0]).toEqual(4);
+      expect(result[1]).toEqual(5);
+      expect(result[2]).toEqual(6);
+      expect(result[3]).toEqual(7);
+      expect(result[4]).toEqual(undefined);
+
+    });
+  });
+  // describe('testing reduce', () => {
+  //   it('should pop off a value from the list', () => {
+  //   });
+  // });
 });
